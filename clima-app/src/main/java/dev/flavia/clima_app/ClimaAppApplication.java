2508a4dfbde5b7104de9488714f49ef06fdb38e4
FlaviaTorresDev <flaviatorresdev@gmail.com>
2024-08-19 21:36:37 -0300

    inicializando o projeto

---
+++ clima-app/src/main/java/dev/flavia/clima_app/ClimaAppApplication.java
@@ -0,0 +1,13 @@
+package dev.flavia.clima_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimaAppApplication.class, args);
+	}
+
+}
---
+++ clima-app/src/test/java/dev/flavia/clima_app/ClimaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.flavia.clima_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
